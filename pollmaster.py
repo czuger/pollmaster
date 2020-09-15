171b34640772fd9448da637fbde29e29511ebe65
ced <zuger.cedric@gmail.com>
2020-09-15 13:38:42 +0200

    Bot schedule moved in it's own file.

--- pollmaster.py
+++ pollmaster.py
@@ -1,22 +1,19 @@
 import json
 import sys
 import traceback
-import threading
 import aiohttp
 import discord
 import logging
 import datetime
-import asyncio
 
 from essentials.messagecache import MessageCache
 from discord.ext import commands
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
-from models.vote_stat import VotesStats
 
 from essentials.multi_server import get_pre
 from essentials.settings import SETTINGS
-from models.poll import Poll
+from utils.poll_schedule import start_scheduled_polls_loop
 
 print(f'Started at {datetime.datetime.now()}', flush=True)
 
@@ -57,78 +54,6 @@ logger.addHandler(ch)
 extensions = ['cogs.config', 'cogs.poll_controls', 'cogs.help', 'cogs.db_api', 'cogs.admin']
 for ext in extensions:
     bot.load_extension(ext)
-
-# logger for scheduled polls
-scheduled_logger = logging.getLogger('scheduled_polls')
-scheduled_logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-sch_fh = logging.FileHandler('logs/scheduled_polls.log',  encoding='utf-8', mode='a+')
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-sch_fh.setFormatter(formatter)
-# add the handlers to the scheduled_logger
-scheduled_logger.addHandler(sch_fh)
-
-last_hour_show_key = None
-
-
-def poll_shown_this_hour(cur_weekday, cur_hour):
-    global last_hour_show_key
-
-    last_hour_key = "{}_{}".format(cur_weekday, cur_hour)
-    scheduled_logger.debug("last_hour_key={}, last_hour_show_key={}".format(last_hour_key, last_hour_show_key))
-    if last_hour_key is None or last_hour_key != last_hour_show_key:
-        last_hour_show_key = last_hour_key
-        scheduled_logger.debug("Poll not shown this hour")
-        return False
-
-    scheduled_logger.debug("Poll shown this hour")
-    return True
-
-
-async def show_poll(poll):
-    """asyncio.sleep is not precise. We can't rely on it."""
-    scheduled_logger.debug('Poll should be launched')
-    scheduled_logger.debug("poll={}".format(poll))
-
-    channel = bot.get_channel(int(poll['channel_id']))
-    scheduled_logger.debug("channel={}".format(channel))
-    await channel.send('Hello')
-
-    poll_from_db = await Poll.load_from_db(bot, poll['server_id'], poll['short'])
-    await poll_from_db.clear_votes()
-
-    await poll_from_db.post_embed(channel)
-    scheduled_logger.debug("Poll posted")
-
-
-async def scheduled_polls_loop():
-    scheduled_logger.debug('In scheduled_polls_loop')
-    while True:
-        await asyncio.sleep(1800)
-
-        now = datetime.datetime.now()
-
-        cur_weekday = now.weekday()
-        cur_hour = now.hour
-
-        scheduled_logger.debug("Schedule log start {}".format(now))
-        scheduled_logger.debug("cur_weekday={}, cur_hour={}".format(cur_weekday, cur_hour))
-
-        if not poll_shown_this_hour(cur_weekday, cur_hour):
-            async for poll in bot.db.polls.find({"scheduled_time": {"$exists": True}}):
-                scheduled_logger.debug(
-                    "poll['short']={}, poll['scheduled_time']={}".format(poll['short'], poll['scheduled_time']))
-
-                sched_weekday = int(poll['scheduled_time']['weekday'])
-                sched_hour = int(poll['scheduled_time']['hour'])
-
-                scheduled_logger.debug("sched_weekday={}, sched_hour={}".format(sched_weekday, sched_hour))
-
-                if cur_weekday == sched_weekday and cur_hour == sched_hour:
-                    show_poll(poll)
-
-        scheduled_logger.debug("Schedule log end")
 
 
 @bot.event
@@ -173,10 +98,9 @@ async def on_ready():
     # async for poll in bot.db.polls.find({}):
     #     print(poll)
 
-    print("Servers verified. Bot running.")
+    start_scheduled_polls_loop()
 
-    loop = asyncio.get_event_loop()
-    loop.create_task(scheduled_polls_loop())
+    print("Servers verified. Bot running.")
 
 
 @bot.event
